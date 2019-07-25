30c5509f2e644af42de2d02a280cfc622251e66d
Jeremy Aube <jeremyaube24@gmail.com>
2019-07-25 11:07:58 -0400

    added npm ignore for jest file and other for safety
    updated tsconfig to allow maps
    
    added vector to base index file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { default as AABB } from './AABB';
 export { default as AABBNode } from './AABBNode';
 export { default as AABBTree } from './AABBTree';
+export { default as Vector3 } from './Vector3'
+export { default as Vector2 } from './Vector2'
+export { default as AABBShapeBase } from './IAABBShape';
